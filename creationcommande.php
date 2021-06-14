b147d41636ec2b9279bd3117ac4dd91906bb67ae
HoussemSaafi <houssemsaafi@insat.u-carthage.tn>
2021-06-14 22:43:00 +0100

    Minor Changes for deployment purpose

--- creationcommande.php
+++ creationcommande.php
@@ -1,13 +1,35 @@
 <?php
 
-include 'livraison.php';
+//include 'livraison.php';
 include 'commande.php';
 if(!isset($_SESSION)) session_start();
 	$c=new Commande();
 	$prix=$c->CalculerPrixTotale();
 	$c->ajouterCommande();
 	$l=new Livraison();
-
-	$l->detailleLivraison("",$_SESSION['adresse'],$_SESSION['user_session']);
-	$l->creerLivraison();
+    l->dateLivraison="";
+    l->adresse=$_SESSION['adresse'];
+    l>idClient=$_SESSION['user_session'];
+	//$l->detailleLivraison("",$_SESSION['adresse'],$_SESSION['user_session']);
+	//$l->creerLivraison();
+    $sql="INSERT into livraison (DateLivraison,etatLivraison,IDCommande,adresse,IDClient)	values(CURDATE(),'Approuvée',".$_SESSION['idCommande'][0].",'".$this->adresse."',".$this->idClient.")";
+			var_dump($sql);
+			$res=$this->conn->query($sql);
+			var_dump($res);
+			if($res)
+            {
+                echo "<br>Votre Commande et livraison ont été ajoutés avec Succes!<br>";
+                $_SESSION['panier']['prixProduit']= array();
+                $_SESSION['panier']['idProduit']=array();
+                $_SESSION['panier']['qte']=array();
+                echo"<a href='index.php'> Rentrez au Shop </a>";
+                //header('');
+            }
+            else{
+                $_SESSION['panier']['prixProduit']= array();
+                $_SESSION['panier']['idProduit']=array();
+                $_SESSION['panier']['qte']=array();
+                echo "<br>livraison non ajoutée<br>";
+                echo"<a href='index.php'> Rentrez au Shop </a>";
+                ;}
 ?>
