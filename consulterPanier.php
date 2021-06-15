30a53342260fb903adcf17604c89adf4bb6e8cdf
emnaboukhris <79046370+emnaboukhris@users.noreply.github.com>
2021-06-15 08:32:41 +0200

    Update consulterPanier.php

--- consulterPanier.php
+++ consulterPanier.php
@@ -47,9 +47,9 @@ if (isset($_SESSION['user_session'])) {
                     {
                         ?>
                         <label class="h5">welcome : <?php print($userRow['username']); ?></label>
-                        <li><a href="/EspaceClient/services/profile.php"><span class="glyphicon glyphicon-user"></span>&nbsp;Espace Client</a></li>
+                        <li><a href="/EspaceClient/services/profile.php"><span class="glyphicon glyphicon-user"></span>&nbsp;Profile</a></li>
 
-                        <li><a href="/EspaceClient/services/logout.php?logout=true"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Se Déconnecter</a></li>
+                        <li><a href="/EspaceClient/services/logout.php?logout=true"><span class="glyphicon glyphicon-log-out"></span>&nbsp;Log Out</a></li>
 
 
                         <?php
@@ -112,10 +112,10 @@ if (isset($_SESSION['user_session'])) {
 	<div class="header_bottom">
 	     	<div class="menu">
 	     		<ul>
-			    	<li><a href="index.php">Acceuil</a></li>
-			    	<li><a href="about.php">A propos</a></li>
-			    	<li><a href="delivery.php">Livraison</a></li>
-			    	<li><a href="news.php">Nouveauté</a></li>
+			    	<li><a href="index.php">Home</a></li>
+			    	<li><a href="about.php">About us</a></li>
+			    	<li><a href="delivery.php">Delivery</a></li>
+			    	<li><a href="news.php">News</a></li>
 			    	<li><a href="contact.php">Reclamation</a></li>
 			    	<div class="clear"></div>
      			</ul>
@@ -342,64 +342,9 @@ foreach ($_SESSION['panier']['idProduit'] as $key => $value) {
 				</div>
 			</div>
         </div>
-				
-   <div class="footer">
-   	  <div class="wrap">	
-	     <div class="section group">
-				<div class="col_1_of_4 span_1_of_4">
-						<h4>Information</h4>
-						<ul>
-                              <li><a href="about.php">About Us</a></li>
-						<li><a href="contact.php">Customer Service</a></li>
-						<li><a href="#">Privacy Policy</a></li>
-						<li><a href="contact.php">Site Map</a></li>
-						<li><a href="#">Search Terms</a></li>
-						</ul>
-					</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<h4>Why buy from us</h4>
-						<ul>
-						<li><a href="about.php">About Us</a></li>
-						<li><a href="contact.php">Customer Service</a></li>
-						<li><a href="#">Privacy Policy</a></li>
-						<li><a href="contact.php">Site Map</a></li>
-						<li><a href="#">Search Terms</a></li>
-						</ul>
-				</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<h4>My account</h4>
-						<ul>
-							<li><a href="contact.php">Sign In</a></li>
-							<li><a href="consulterPanier.php">View Cart</a></li>
-							<li><a href="#">My Wishlist</a></li>
-							<li><a href="#">Track My Order</a></li>
-							<li><a href="contact.php">Help</a></li>
-						</ul>
-				</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<h4>Contact</h4>
-						<ul>
-							<li><span>+216 26 211 344</span></li>
-							<li><span>			<li><span></span></li></span></li>
-						</ul>
-						<div class="social-icons">
-							<h4>Follow Us</h4>
-					   		  <ul>
-							      <li><a href="https://www.facebook.com/Shipping-from-across-the-World-111456524500065" target="_blank"><img src="web/images/facebook.png" alt="" /></a></li>
-							      <li><a href="#" target="_blank"><img src="web/images/twitter.png" alt="" /></a></li>
-							      <li><a href="#" target="_blank"><img src="web/images/skype.png" alt="" /> </a></li>
-							      <li><a href="#" target="_blank"> <img src="web/images/dribbble.png" alt="" /></a></li>
-							      <li><a href="#" target="_blank"> <img src="web/images/linkedin.png" alt="" /></a></li>
-							      <div class="clear"></div>
-						     </ul>
-   	 					</div>
-				</div>
-			</div>			
-        </div>
-        <div class="copy_right">
-				<p>Gl2-2020/2021</a> </p>
-		   </div>
-    </div>
+	  <?php   require("footerLayout.php");  ?>
+			
+  
    <script type="text/javascript">
 		$(document).ready(function() {			
 			$().UItoTop({ easingType: 'easeOutQuart' });
