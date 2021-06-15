b52c2588542361b5e03d17f899db11bbbd1f14d2
emnaboukhris <79046370+emnaboukhris@users.noreply.github.com>
2021-06-15 07:45:22 +0200

    Update about.php

--- about.php
+++ about.php
@@ -196,70 +196,8 @@ include 'raccourciPanier.php';
     </div>
  </div>
 </div>
-   <div class="footer">
-   	  <div class="wrap">
-	     <div class="section group">
-				<div class="col_1_of_4 span_1_of_4">
-                    <h4>Information</h4>
-                    <ul>
-                        <li><a href="about.php"<li><span>
-                        <li><span</li></span></li>">About Us"</a></li>
-                        <li><a href="contact.php">Customer Service</a></li>
-                        <li><a href="#">Advanced Search</a></li>
-                        <li><a href="delivery.php">Orders and Returns</a></li>
-                        <li><a href="contact.php">Contact Us</a></li>
-                    </ul>
-					</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<h4>Why buy from us</h4>
-						<ul>
-						<li><a href="about.php
-							<li><span>			<li><span></span></li></span></li>">About Us</a></li>
-						<li><a href="contact.php
-							<li><span>			<li><span></span></li></span></li>">Customer Service</a></li>
-						<li><a href="#">Privacy Policy</a></li>
-						<li><a href="contact.php
-							<li><span>			<li><span></span></li></span></li>">Site Map</a></li>
-						<li><a href="#">Search Terms</a></li>
-						</ul>
-				</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<h4>My account</h4>
-						<ul>
-							<li><a href="contact.php
-							<li><span>			<li><span></span></li></span></li>">Sign In</a></li>
-							<li><a href="index.php
-							<li><span>			<li><span></span></li></span></li>">View Cart</a></li>
-							<li><a href="#">My Wishlist</a></li>
-							<li><a href="#">Track My Order</a></li>
-							<li><a href="contact.php
-							<li><span>			<li><span></span></li></span></li>">Help</a></li>
-						</ul>
-				</div>
-				<div class="col_1_of_4 span_1_of_4">
-					<h4>Contact</h4>
-						<ul>
-							<li><span>+216 26 211 344</span></li>
-							<li><span>			<li><span></span></li></span></li>
-						</ul>
-						<div class="social-icons">
-							<h4>Follow Us</h4>
-					   		  <ul>
-							      <li><a href="https://www.facebook.com/Shipping-from-across-the-World-111456524500065" target="_blank"><img src="web/images/facebook.png" alt="" /></a></li>
-							      <li><a href="#" target="_blank"><img src="web/images/twitter.png" alt="" /></a></li>
-							      <li><a href="#" target="_blank"><img src="web/images/skype.png" alt="" /> </a></li>
-							      <li><a href="#" target="_blank"> <img src="web/images/dribbble.png" alt="" /></a></li>
-							      <li><a href="#" target="_blank"> <img src="web/images/linkedin.png" alt="" /></a></li>
-							      <div class="clear"></div>
-						     </ul>
-   	 					</div>
-				</div>
-			</div>
-        </div>
-        <div class="copy_right">
-				<p>GL2 2020/2021 © All rights Reseverd |</p>
-		   </div>
-    </div>
+  <?php   require("footerLayout.php");  ?>
+
    <script type="text/javascript">
 		$(document).ready(function() {			
 			$().UItoTop({ easingType: 'easeOutQuart' });
